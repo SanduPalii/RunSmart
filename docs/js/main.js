34450538d9e27f3334150c5816a798d2de414631
Sandu Palii <palii.sandu@gmail.com>
2022-06-15 15:52:17 +0300

    adding carousel

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,20 @@
-$(function () {
+const swiper = new Swiper('.swiper', {
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  centeredSlides: true,
+  
+  loop: true,
 
-})
+  zoom: true,
+
+  autoHeight: true,
+
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  }
+
+});
