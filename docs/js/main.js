ae0baace2ab7652178985ff235d93fa545c6a5b2
Sandu Palii <palii.sandu@gmail.com>
2022-07-13 13:04:34 +0300

    index/ block catalog(+js)

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,20 +1,47 @@
 const swiper = new Swiper('.swiper', {
   // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  
   centeredSlides: true,
   
   loop: true,
 
-  zoom: true,
-
-  autoHeight: true,
+  autoHeight: false,
 
   autoplay: {
-    delay: 2500,
+    delay: 4500,
     disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+$(function() {
+  
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+  });
+
+  function toggleSlide(item) {
+    $(item).each(function(i){
+      $(this).on('click', function(e){
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    })
   }
 
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
 });
