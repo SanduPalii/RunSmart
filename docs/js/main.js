0202fe633e484d726c29fbbb44909627cf0c4456
Sandu Palii <palii.sandu@gmail.com>
2022-07-14 12:03:04 +0300

    index/ forms validation, jquery.validate.min.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,5 @@
+// Navigation arrows
 const swiper = new Swiper('.swiper', {
-  // Navigation arrows
-  
   centeredSlides: true,
   
   loop: true,
@@ -25,13 +24,14 @@ const swiper = new Swiper('.swiper', {
 });
 
 $(function() {
-  
+  // Catalog tabs
   $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
     $(this)
       .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
 
+  //in card button - more details link >< back
   function toggleSlide(item) {
     $(item).each(function(i){
       $(this).on('click', function(e){
@@ -41,7 +41,56 @@ $(function() {
       })
     })
   }
-
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
+
+  // Modal
+  $('[data-modal=consultation').on('click', function(){
+    $('.overlay, #consultation').fadeIn('slow');
+  })
+
+  //buttons - close popup
+  $('.modal__close').on('click', function(){
+    $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+  })
+
+  // //buttons - buy now
+  // $('.button_mini').on('click', function(){
+  //   $('.overlay, #order').fadeIn('slow');
+  // })
+
+  //separate element in buy now card
+  $('.button_mini').each(function(i){
+    $(this).on('click', function(){
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    })
+  })
+
+  //jquery validate form
+  function validateForms(form){
+    $(form).validate({
+      rules:{
+        name: "required",
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: "Как к вам обращаться?",
+        phone: "Введите свой номер телефона",
+        email: {
+          required: "Напишите свой емайл",
+          email: "Введите правильно почтовый адрес"
+        }
+      }
+    });
+  }
+
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
+  
 });
